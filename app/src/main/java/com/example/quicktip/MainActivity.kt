fa0ca431cad134908720dd9b6b7c353999cec748
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-02 21:17:49 +0800

    [feat] challenge chapter 2

--- app/src/main/java/com/example/quicktip/MainActivity.kt
+++ app/src/main/java/com/example/quicktip/MainActivity.kt
@@ -1,46 +1,29 @@
 package com.example.quicktip
 
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.quicktip.ui.theme.QuicktipTheme
+import android.view.View
+import com.example.quicktip.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            QuicktipTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener(this)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.calculate_button -> {
+                binding.image.setImageDrawable(getDrawable(R.drawable.img_thanks))
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuicktipTheme {
-        Greeting("Android")
     }
 }
